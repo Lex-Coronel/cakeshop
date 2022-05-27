d3a4b461d11879ffcfba385f8894631580fd8564
Lex-Coronel <coronellexmarcus@gmail.com>
2022-05-27 15:14:36 +0800

    routing

--- huebschbyjan/store/views.py
+++ huebschbyjan/store/views.py
@@ -1,3 +1,27 @@
+from multiprocessing import context
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    context = {}
+    return render(request,'store/home.html')
+
+def login(request):
+    context = {}
+    return render(request,'store/login.html')
+
+def register(request):
+    context = {}
+    return render(request,'store/register.html')
+
+def payment(request):
+    context = {}
+    return render(request,'store/payment.html')
+
+def item(request):
+    context = {}
+    return render(request,'store/item.html')
+
+def contact(request):
+    context = {}
+    return render(request,'store/contact-us.html')
+
